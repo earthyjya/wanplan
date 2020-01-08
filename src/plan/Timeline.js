a450f58150248c7aecabc7236ea64620f38e0642
oatsty <parinayoksatayu@gmail.com>
2020-01-09 03:04:42 +0900

    number of day changed dynamically

--- src/plan/Timeline.js
+++ src/plan/Timeline.js
@@ -4,21 +4,26 @@ import Request from "../lib/Request";
 
 class Timeline extends React.Component {
   state = {
-    serverIP: ""
-    
+    serverIP: "",
+    day : 1
   };
 
   componentDidMount() {
     this.setState({serverIP: this.props.serverIP})
   }
+
+  addDay = () => {
+    this.props.changeDays(this.props.days.length + 1)
+  }
   render() {
 
     return (
       <div className="Timeline">
-        <h1>Your wonderful Trip !!!!!!</h1>
+  <h2>Day {this.props.day}</h2>
         <Request url= {this.props.serverIP + ":3030/trip_detail"}>
                 {result => <AttCardList {...result} serverIP = {this.props.serverIP} />}
         </Request>
+        <button onClick = {this.addDay}>+</button>
       </div>
     );
   }
