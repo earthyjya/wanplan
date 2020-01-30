0c8baa6c1dbcec0907855bd8eefb46f3a92251b0
mdechdee <dechdee.m@gmail.com>
2020-01-30 11:25:41 +0700

    Update post's overview tab, more styling!

--- src/posts/PostDetail.js
+++ src/posts/PostDetail.js
@@ -4,6 +4,7 @@ import { Card, CardImg, CardTitle, CardText, CardBody, CardSubtitle, Row, Col, C
 // import withRequest from "../lib/withRequest"
 import {Link} from "react-router-dom";
 import classnames from 'classnames';
+import "./PostDetail.css"
 
 class PostDetail extends Component {
 	constructor(props){
@@ -20,9 +21,11 @@ class PostDetail extends Component {
 	render() {
 		return (
 			<React.Fragment>
-				<Jumbotron style={{"backgroundImage": "url(https://d3hne3c382ip58.cloudfront.net/resized/1920x700/japan-tours-400X400_.JPG)"}}>
-					<h1>Japan</h1>
-				</Jumbotron>
+				<div
+					className="post-header"
+					style={{"backgroundImage": "url(https://d3hne3c382ip58.cloudfront.net/resized/1920x700/japan-tours-400X400_.JPG)"}}
+					>
+				</div>
 				<Nav tabs>
 					<NavItem>
 						<NavLink
@@ -40,25 +43,39 @@ class PostDetail extends Component {
 							Details
 						</NavLink>
 					</NavItem>
+					<NavItem>
+						<NavLink
+							className={classnames({ active: this.state.activeTab === '3' })}
+							onClick={() => { this.toggle('3'); }}
+							>
+							Map
+						</NavLink>
+					</NavItem>
 				</Nav>
 				<TabContent activeTab={this.state.activeTab}>
-					<TabPane tabId="1">
+					<TabPane tabId="1" className="tab-content">
 							<Row>
-								<Col>Plan description</Col>
+								<Col sm={12} md={6} lg={7} className="overview-description-wrap">
+										<h2>Post Name</h2>
+										Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+								</Col>
 								<Col>
 									<Card>
-										 <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+
+										 	<img
+												 src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png"
+												 className="avatar-image"
+												 />
+
 										 <CardBody>
-											 <CardTitle>Card title</CardTitle>
-											 <CardSubtitle>Card subtitle</CardSubtitle>
-											 <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-											 <Button>Button</Button>
+											 <CardTitle>John Doe</CardTitle>
+											 <CardSubtitle>User description</CardSubtitle>
 										 </CardBody>
 									 </Card>
 								</Col>
 							</Row>
 					</TabPane>
-					<TabPane tabId="2">
+					<TabPane tabId="2" className="tab-content">
 
 					</TabPane>
 			 </TabContent>
