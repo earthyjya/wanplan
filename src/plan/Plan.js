298fe4c3a0d4cfbb2a129d6331f2cc2c2505de60
mdechdee <dechdee.m@gmail.com>
2020-02-02 13:19:24 +0700

    provide plan description

--- src/plan/Plan.js
+++ src/plan/Plan.js
@@ -7,6 +7,7 @@ import Request from "../lib/Request.js";
 import AttBar from "./AttBar.js";
 import { DragDropContext } from "react-beautiful-dnd";
 import { Row, Col, Container, Button } from "reactstrap";
+import "./Plan.css"
 
 class Plan extends React.Component {
   state = {
@@ -261,9 +262,14 @@ class Plan extends React.Component {
       return (
         <React.Fragment>
           <div
-  					className="post-header"
+  					className="plan-header"
   					style={{"backgroundImage": "url(https://d3hne3c382ip58.cloudfront.net/resized/1920x700/japan-tours-400X400_.JPG)"}}
-  				/>
+  				>
+            <Container fluid className="plan-description-container">
+              <div className="plan-description-title"> {trip_overview.trip_name} </div>
+              <div className="plan-description"> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
+            </Container>
+          </div>
           <div className="title-bar">
             <div className="city">{city.city_name}</div>
             <div className="title">{trip_overview.trip_name}</div>
