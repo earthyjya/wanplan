aa2c6fc5b34dd48fceed43361b0880276b1876e5
earthyjya <vittayakoonsakulchai.n.aa@m.titech.ac.jp>
2020-02-24 13:36:21 +0700

    temporary transcard design added

--- src/plan/TransCard.js
+++ src/plan/TransCard.js
@@ -1,15 +1,22 @@
 import React, { Component } from "react";
+import "./TransCard.css";
 
 class TransCard extends Component {
   render() {
     const { start, destination } = this.props;
     return (
-      <div>
-        <div>transport</div>
-        <div>
-          From {start} to{" " + destination}
+     <div className="Transcard">
+        <div className="transport">transport  </div>
+        <div className="from">
+           From
         </div>
+        <div className="start">{start} </div>
+        <div className="to">  to </div>
+        <div className="destination">{" " + destination}</div>
+
+
       </div>
+
     );
   }
 }
