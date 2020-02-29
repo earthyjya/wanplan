70471d61876b52b2eceaf21e968dabb4bbcbdc9d
Janat Taerakul <janat.taerakul@gmail.com>
2020-02-29 13:06:42 +0700

    Server can handle disconnection

--- Wanplan_API_1.2.0/models/db.js
+++ Wanplan_API_1.2.0/models/db.js
@@ -2,17 +2,35 @@ const mysql = require("mysql");
 const dbConfig = require("../config/db.config.js");
 
 // Create a connection to the database
-const connection = mysql.createConnection({
-  host: dbConfig.host,
-  user: dbConfig.user,
-  password: dbConfig.password,
-  database: dbConfig.database
+let connection = mysql.createConnection({
+	host: dbConfig.host,
+	user: dbConfig.user,
+	password: dbConfig.password,
+	database: dbConfig.database
 });
+
+handleDisconnect(connection);
 
 // open the MySQL connection
 connection.connect(error => {
-  if (error) throw error;
-  console.log("Successfully connected to the database.");
+	if (error) throw error;
+	console.log("Successfully connected to the database.");
 });
+
+function handleDisconnect(client) {
+	client.on("error", function(error) {
+		if (!error.fatal) return;
+		if (error.code !== "PROTOCOL_CONNECTION_LOST") throw err;
+
+		console.error("> Re-connecting lost MySQL connection: " + error.stack);
+
+		// NOTE: This assignment is to a variable from an outer scope; this is extremely important
+		// If this said `client =` it wouldn't do what you want. The assignment here is implicitly changed
+		// to `global.connection =` in node.
+		connection = mysql.createConnection(client.config);
+		handleDisconnect(connection);
+		connection.connect();
+	});
+}
 
 module.exports = connection;
