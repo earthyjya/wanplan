d57079c4fa62ab26ca68b613f08e4e6f53b8cec1
Janat Taerakul <janat.taerakul@gmail.com>
2020-03-01 09:21:23 +0700

    Lat one was not working

--- Wanplan_API_1.2.0/models/db.js
+++ Wanplan_API_1.2.0/models/db.js
@@ -2,35 +2,19 @@ const mysql = require("mysql");
 const dbConfig = require("../config/db.config.js");
 
 // Create a connection to the database
-let connection = mysql.createConnection({
+let pool = mysql.createPool({
 	host: dbConfig.host,
 	user: dbConfig.user,
 	password: dbConfig.password,
 	database: dbConfig.database
 });
 
-handleDisconnect(connection);
-
-// open the MySQL connection
-connection.connect(error => {
-	if (error) throw error;
-	console.log("Successfully connected to the database.");
+pool.getConnection(function(err, connection) {
+	if (err) throw err; // not connected!
+	else {
+		console.log("Successfully connected to the database.");
+		connection.release();
+	}
 });
 
-function handleDisconnect(client) {
-	client.on("error", function(error) {
-		if (!error.fatal) return;
-		if (error.code !== "PROTOCOL_CONNECTION_LOST") throw err;
-
-		console.error("> Re-connecting lost MySQL connection: " + error.stack);
-
-		// NOTE: This assignment is to a variable from an outer scope; this is extremely important
-		// If this said `client =` it wouldn't do what you want. The assignment here is implicitly changed
-		// to `global.connection =` in node.
-		connection = mysql.createConnection(client.config);
-		handleDisconnect(connection);
-		connection.connect();
-	});
-}
-
-module.exports = connection;
+module.exports = pool;
