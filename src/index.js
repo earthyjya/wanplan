786cd7a4ed1e19e008f2cb2fa8a8cebbed6dd400
earthyjya <vittayakoonsakulchai.n.aa@m.titech.ac.jp>
2020-04-23 21:52:00 +0900

    add google analytic and google tag manager

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./scss/index.scss";
 import App from "./App";
+import TagManager from 'react-gtm-module'
 import "bootstrap/dist/css/bootstrap.min.css";
 import './scss/fonts/NotoSansThai-Regular.ttf'
 // import * as serviceWorker from "./serviceWorker";
 
+const tagManagerArgs = {
+    gtmId: 'GTM-TZHMJ9Z'
+}
+ 
+TagManager.initialize(tagManagerArgs)
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
