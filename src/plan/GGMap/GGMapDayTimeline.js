e56efea6a23d54afdd26500ae3413065f2a0701a
mdechdee <dechdee.m@gmail.com>
2020-07-31 05:18:27 +0900

    a bit better styling GGMap page

--- src/plan/GGMap/GGMapDayTimeline.js
+++ src/plan/GGMap/GGMapDayTimeline.js
@@ -1,5 +1,27 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 class GGMapDayTimeline extends Component {
+	getTransportComponent = (transport) => {
+		if(transport && transport.mode){
+			return (
+				<div className="ggmap-transport-container">
+					<div><FontAwesomeIcon icon="car" size="sm" /></div>
+					<div>{ transport.mode}</div>
+				</div>
+			)
+		}
+		else{
+			return <div className="ggmap-transport-container"> No transport </div>
+		}
+	}
+	getAttComponent = (detail) =>{
+		return(
+			<div className="ggmap-att-container">
+				<div><FontAwesomeIcon icon="camera" size="sm" /></div>
+				<div>{ detail.attraction_name}</div>
+			</div>
+		)
+	}
 	render() {
 		const { plan_detail, day, editing, transports } = this.props;
 		let start = "";
@@ -13,11 +35,10 @@ class GGMapDayTimeline extends Component {
 					(() => {
 						let att_show = []
 						let idx = 0
-						let getTransport = (t) => {if(t && t.mode){return t.mode} else{return "No transport"}}
 						plan_detail.map(detail => {
-							if(idx === 0) att_show.push(<div>{getTransport(transports[day-1][idx])}</div>)
-							att_show.push(<div className="ggmap-att-name">{detail.attraction_name}</div>)
-							att_show.push(<div>{getTransport(transports[day-1][idx+1])}</div>)
+							if(idx === 0) att_show.push(this.getTransportComponent(transports[day-1][idx]))
+							att_show.push(this.getAttComponent(detail))
+							att_show.push(this.getTransportComponent(transports[day-1][idx+1]))
 							idx = idx + 1
 						})
 						return att_show
