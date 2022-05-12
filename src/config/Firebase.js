fa05dcf915b10eddfaa6eeb78174ec9ed60ce004
Janat Taerakul <janat.taerakul@gmail.com>
2022-05-13 02:31:24 +0900

    Update Firebase.js

--- src/config/Firebase.js
+++ src/config/Firebase.js
@@ -1,11 +1,11 @@
 import firebase from "firebase";
 
   const Config = {
-    apiKey: "AIzaSyCsu5b7hxz2L91C71SRLooslZI8Dhb_tYU",
-    authDomain: "oneplan-803eb.firebaseapp.com",
-    databaseURL: "https://oneplan-803eb.firebaseio.com",
-    projectId: "oneplan-803eb",
-    storageBucket: "oneplan-803eb.appspot.com",
+    apiKey: "Firebase api key",
+    authDomain: "Firebase auth domain",
+    databaseURL: "Firebase database URL",
+    projectId: "Firebase Project ID",
+    storageBucket: "Firebase Storage Bucket",
     messagingSenderId: "501104765863",
     appId: "1:501104765863:web:fd41fcac0a4a59a4032482",
     measurementId: "G-J12YS1K8PJ"
